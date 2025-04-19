6b143e43d4ce428b7e77b283c1ad3c24c16a8709
Brice HUET <brice.huet@epitech.eu>
2025-04-19 10:44:26 +0100

    add github page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/ekart-website/', // 👈 Ajoute cette ligne
   plugins: [react()],
 })
