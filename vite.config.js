fa4de26ec247750eeb5b1a70d29a05f611dac877
Brice HUET <brice.huet@epitech.eu>
2025-05-05 22:05:48 +0100

    fix 404 error

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Site_vitrine/', // ⚠️ Le nom exact du repo ici
+  base: '/', // ⚠️ Le nom exact du repo ici
   plugins: [react()],
 })
